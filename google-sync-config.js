2d8e40d169c8ac5296ee73e36499a61722a8e142
gurkenfurzi <muelliaccc@gmail.com>
2026-09-04 22:21:36 +0200

    Update google-sync-config.js

--- google-sync-config.js
+++ google-sync-config.js
@@ -1,5 +1,5 @@
 // Google Apps Script Web-App-URL nach dem Bereitstellen hier eintragen.
 // Beispiel: https://script.google.com/macros/s/AKfycb.../exec
 window.GOOGLE_SYNC_CONFIG = {
-  scriptUrl: "DEINE_GOOGLE_APPS_SCRIPT_WEBAPP_URL"
+  scriptUrl: "https://script.google.com/macros/s/AKfycbwqe20wXrAZ7Gjy3jWqEXjg4KcSRSEehG971yXfMlVdPW0az2Fw6I7NnmnzqgQa22we/exec"
 };
